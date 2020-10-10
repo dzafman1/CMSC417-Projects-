e348348ea8245cf9902971f47b42db85ab7ee1bb
Student McStudent <student@cmsc417.home>
2020-10-10 10:27:21 -0400

    a2 r2

--- assignment2/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-truncate -s 0 node0
-truncate -s 0 node1
-truncate -s 0 node2
-truncate -s 0 node3
-script -c "sudo ./rt -n 0" node0 & script -c "sudo ./rt -n 1" node1 & script -c "sudo ./rt -n 2" node2 & script -c "sudo ./rt -n 3" node3
-
